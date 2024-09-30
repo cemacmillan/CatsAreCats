cb3e6ad8a9fbdaf11fa1fddcb0d0cb6af1f12a61
C Ewen Mac MILLAN <youenmacmillan@gmail.com>
2024-09-30 13:42:13 +0200

    Update CatsAreCatsMod.cs
    Correct version string

--- Source/CatsAreCatsMod.cs
+++ Source/CatsAreCatsMod.cs
@@ -13,7 +13,7 @@ namespace DIL_CatsAreCats
             settings = GetSettings<CatsAreCatsModSettings>();
             var harmony = new Harmony("cem.catsarecats");
             harmony.PatchAll();
-            Log.Message("<color=#00FF7F>[Cats Are Cats But Their Eyes Are Charge-Rifles And Their Claws Are Power Claws And They Regenerate]</color> v1.5.4 dajjaj-bing");
+            Log.Message("<color=#00FF7F>[Cats Are Cats But Their Eyes Are Charge-Rifles And Their Claws Are Power Claws And They Regenerate]</color> v1.5.4 dajjaj-kalamari");
         }
 
         public override void DoSettingsWindowContents(Rect inRect)
